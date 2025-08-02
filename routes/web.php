c3e25d5049b52247c50c0e747d4c17be4dc020af
Zaelani <bahar32harz@gmail.com>
2025-08-02 22:01:41 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TransaksiController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,3 +9,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [TransaksiController::class, 'create'])->name('transaksi.create');
 Route::post('/', [TransaksiController::class, 'store'])->name('transaksi.store');
 Route::get('/nota/{transaksi}', [TransaksiController::class, 'show'])->name('transaksi.show');
+
+// Route for clearing cache, config, and routes
+Route::get('/clear-cache', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    return 'Cache, config, and routes cleared successfully!';
+})->name('clear.cache');
